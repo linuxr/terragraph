aabf53a5d6eba844de854fc750b6aa48b2db086f
wangyubin <linuxr@aliyun.com>
2022-07-01 00:18:37 +0800

    add command and flags

---
+++ cmds/root.go
@@ -0,0 +1,37 @@
+package cmds
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	inputPath  string
+	provider   string
+	configPath string
+
+	rootCmd = &cobra.Command{
+		Use: "terragraph",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Printf("input: %s\n", inputPath)
+			fmt.Printf("provider: %s\n", provider)
+			fmt.Printf("config: %s\n", configPath)
+			return nil
+		},
+	}
+)
+
+func Execute() {
+	rootCmd.Execute()
+}
+
+func init() {
+	rootCmd.Flags().StringVarP(&inputPath, "input", "i", "", "输入文件的路径(HCL, tfplan.json, tfstate.json)")
+
+	rootCmd.Flags().StringVar(&provider, "provider", "alicloud", "provider 类型")
+	rootCmd.Flags().StringVarP(&configPath, "config", "c", "", "配置文件的路径")
+
+	rootCmd.MarkFlagRequired("input")
+	rootCmd.MarkFlagRequired("config")
+}
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"github.com/linuxr/terragraph/cmds"
+)
 
 func main() {
-	fmt.Println("hello terragraph")
+	cmds.Execute()
 }
