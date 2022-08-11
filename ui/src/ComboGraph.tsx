997cfa0db77d59977021e4b9e41d8adfc41c2e6e
wangyubin <linuxr@aliyun.com>
2022-08-12 01:07:18 +0800

    修改生成data.json中的名称

---
+++ ui/src/ComboGraph.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect } from "react";
+import G6 from "@antv/g6";
+
+const ComboGraph = (props: { data: any }) => {
+    const { data } = props;
+
+    useEffect(() => {
+        const container = document.getElementById("container");
+        const width = container?.scrollWidth;
+        const height = (container?.scrollHeight || 500) - 30;
+
+        const graph = new G6.Graph({
+            container: "container",
+            width,
+            height: height - 50,
+            fitView: true,
+            fitViewPadding: 30,
+            animate: true,
+            groupByTypes: false,
+            modes: {
+                default: [
+                    "drag-combo",
+                    "drag-node",
+                    "drag-canvas",
+                    {
+                        type: "collapse-expand-combo",
+                        relayout: false,
+                    },
+                ],
+            },
+            layout: {
+                type: "dagre",
+                sortByCombo: false,
+                ranksep: 10,
+                nodesep: 10,
+            },
+            defaultNode: {
+                size: [60, 30],
+                type: "rect",
+                anchorPoints: [
+                    [0.5, 0],
+                    [0.5, 1],
+                ],
+            },
+            defaultEdge: {
+                type: "line",
+            },
+            defaultCombo: {
+                type: "rect",
+                style: {
+                    fillOpacity: 0.1,
+                },
+            },
+        });
+        graph.data(data);
+        graph.render();
+    });
+
+    return <div id="container" />;
+};
+
+export default ComboGraph;
